a6a59260fd46615b4be127a66d20558fbf37b657
seye2 <seye2@nate.com>
2018-05-08 17:56:31 +0900

    zip파일 추가 및 람다 index파일 경로 변경

--- config/zip.js
+++ config/zip.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-const zip = new require('node-zip')();
+// const zip = new require('node-zip')();
+const zipFolder = require('zip-folder');
 
-
-zip.file('index.js', fs.readFileSync(path.join(__dirname, '../functions/reactssr/server/*')));
-var data = zip.generate({base64:false,compression:'DEFLATE'});
-fs.writeFileSync('../functions/reactssr/reactssr.zip', data, 'binary');
-
-
+zipFolder('./functions/reactssr/server', './functions/reactssr/server.zip', function(err) {
+	if(err) {
+		console.log('oh no!', err);
+	} else {
+		console.log('EXCELLENT');
+	}
+});
+//
+// zip.file('index.js', fs.readFileSync(path.join(__dirname, '../functions/reactssr/server/**')));
+// var data = zip.generate({base64:false,compression:'DEFLATE'});
+// fs.writeFileSync('../functions/reactssr/reactssr.zip', data, 'binary');
+//
+//
